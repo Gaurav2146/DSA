f2fea0a31f62c73c48aeb11996c7f4a6af1ef344
gaurav2146 <gaurav2146.gk@gmail.com>
2023-09-10 23:51:28 +0530

    changes done

---
+++ String/Split_the_binary_string_into_substrings_with_equal_number_of_0s_and_1s.ts
@@ -0,0 +1,5 @@
+//Given a binary string str of length N, the task is to find the maximum count of consecutive 
+//substrings str can be divided into such that all the substrings are balanced i.e. they have 
+//equal number of 0s and 1s. If it is not possible to split str satisfying the conditions then
+// print -1.
+
