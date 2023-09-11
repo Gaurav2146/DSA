3db45c83cae76e044894a9c325462d09bf6c5e64
Gaurav Kumar <GauravKumar@WinjitTechnologiesPvtLtd.local>
2023-09-11 11:11:36 +0530

    changes done

---
+++ String/CheckWhetherOneStringIsRotationOfAnotherString.ts
@@ -0,0 +1,4 @@
+// let s1 = ABCDE and s2 = DEABC
+// Find if s2 is rotation of s1.
+// Approach:- Concatinate s1 with s1 and find if s2 is substring of resultant concatenated string.
+// If s2 is substring of resultant concatenated string then s2 is rotation of s1.
