01104053a2a4a57e6c55973307ecf72f218f7106
Gaurav Kumar <GauravKumar@WinjitTechnologiesPvtLtd.local>
2023-09-11 11:25:53 +0530

    changes done

---
+++ String/validSuffle.ts
@@ -0,0 +1,3 @@
+//Input will be two strings s1=”abc” and s2=“xyz” and s3 = “axbycz”.
+//We have to find whether s3 is a valid shuffle of s1 and s2.
+//Valid shuffle means s1 and s2 should be present in the same order in s3 as it is present originally.
