b6c96ecd32e17babea1a691cc59194718c64f8bf
Gaurav Kumar <GauravKumar@WinjitTechnologiesPvtLtd.local>
2023-09-14 14:38:57 +0530

    changes done

--- DP/palindromPartitioning.ts
+++ DP/palindromPartitioning.ts
@@ -3,82 +3,60 @@ function minCut(s: string): number {
     let palindomDP: number[][] = [];
 
     for (let i = 0; i < s.length; i++) {
-        let arr = []
+        let arr = [];
+
         for (let j = 0; j < s.length; j++) {
-            arr.push(0);
+            arr.push(Number.MAX_SAFE_INTEGER);
         }
         palindomDP.push(arr);
     }
 
     claculatePalindromicSubstrings(s, palindomDP);
 
-    if (palindomDP[0][s.length - 1] > 0) {
+    if (palindomDP[0][s.length - 1] == 0) {
         return 0;
     }
 
-    for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < s.length; j++) {
-            if (palindomDP[i][j] == 0) {
-                palindomDP[i][j] = Number.MAX_SAFE_INTEGER;
-            }
-            else if (palindomDP[i][j] > 0) {
-                palindomDP[i][j] = 0;
-            }
-        }
+    let min_cut = [];
 
+    for (let i = 0; i < s.length; i++) {
+        min_cut.push(Number.MAX_SAFE_INTEGER);
     }
 
-
-    let DP: number[][] = [];
+    //"aab"
 
     for (let i = 0; i < s.length; i++) {
-        let arr = []
-        for (let j = 0; j < s.length; j++) {
-            arr.push(Number.MAX_SAFE_INTEGER);
+
+        if (palindomDP[0][i] == 0) {
+            min_cut[i] = 0;
         }
-        DP.push(arr);
-    }
+        else {
 
-
-
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i; j < s.length; j++) {
-
-            if (palindomDP[i][j] == 0) {
-                DP[i][j] = 0;
-            }
-            else if (DP[i][j] == Number.MAX_SAFE_INTEGER) {
-
-                let min = Number.MAX_SAFE_INTEGER;
-
-                for (let k = i; k < j; k++) {
-
-                    let temp = 1 + Math.min(DP[i][k], palindomDP[i][k]) + Math.min(DP[k + 1][j], palindomDP[k + 1][j]);
-
-                    if (temp < min) {
-                        min = temp;
+            for (let j = i; j >= 1; j--) {
+                if (palindomDP[j][i] == 0) {
+                    if (min_cut[j - 1] + 1 < min_cut[i]) {
+                        min_cut[i] = min_cut[j - 1] + 1;
                     }
+
                 }
-
-                DP[i][j] = Math.min(min, DP[i][j]);
             }
-        }
 
+        }
     }
 
-    return DP[0][s.length - 1];
+    return min_cut[s.length - 1];
+}
 
-};
 
 function claculatePalindromicSubstrings(s: string, palindromicDP: number[][]) {
 
     for (let i = 0; i < s.length; i++) {
-        palindromicDP[i][i] = 1;
+        palindromicDP[i][i] = 0;
     }
 
     for (let i = 0; i < s.length - 1; i++) {
         if (s[i] == s[i + 1]) {
-            palindromicDP[i][i + 1] = 2;
+            palindromicDP[i][i + 1] = 0;
         }
 
     }
@@ -86,13 +64,13 @@ function claculatePalindromicSubstrings(s: string, palindromicDP: number[][]) {
     for (let i = s.length - 1 - 2; i >= 0; i--) {
         for (let j = i + 2; j < s.length; j++) {
 
-            if (s[i] == s[j] && palindromicDP[i + 1][j - 1] > 0) {
+            if (s[i] == s[j] && palindromicDP[i + 1][j - 1] == 0) {
 
-                palindromicDP[i][j] = 2 + palindromicDP[i + 1][j - 1];
+                palindromicDP[i][j] = palindromicDP[i + 1][j - 1];
 
             }
             else {
-                palindromicDP[i][j] = 0;
+                palindromicDP[i][j] = Number.MAX_SAFE_INTEGER;
             }
 
         }
