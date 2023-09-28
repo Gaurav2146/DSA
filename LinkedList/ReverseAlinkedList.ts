33a69c820feb617ac62c6c1250387eb75f6be3eb
gaurav2146 <gaurav2146.gk@gmail.com>
2023-09-28 22:16:21 +0530

    changes done

---
+++ LinkedList/ReverseAlinkedList.ts
@@ -0,0 +1,36 @@
+
+//Definition for singly-linked list.
+class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+     }
+ }
+ 
+
+function reverseList(head: ListNode | null): ListNode | null {
+
+    if(head === null)
+    {
+        return head;
+    }
+    
+    let prevNode = head;
+    let nextNode = head.next;
+    
+    head.next = null;
+    
+    while(nextNode != null)
+    {
+    let nextVal = nextNode.next;    
+    nextNode.next = prevNode;
+    
+    prevNode =  nextNode;
+    nextNode = nextVal;
+    }
+        
+    head = prevNode;
+    return head;
+    };
