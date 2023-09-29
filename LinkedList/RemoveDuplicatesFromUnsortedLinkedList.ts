368a0a3046685ebd3a71ab1b69b19b46a41a9690
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-09-29 13:37:59 +0530

    changes done

---
+++ LinkedList/RemoveDuplicatesFromUnsortedLinkedList.ts
@@ -0,0 +1,66 @@
+//User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {Node}
+*/
+
+
+class LinkedListNode {
+    data: number;
+    next: LinkedListNode | null;
+    constructor(data: number) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class RemoveDuplicatesFromUnsortedLinkedList {
+    //Function to remove duplicates from unsorted linked list.
+    removeDuplicates(head: LinkedListNode | null): LinkedListNode | null {
+        //your code here
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        let set = new Set();
+
+        let prev = null;
+        let curr = head;
+        let next = head.next;
+
+        while (curr != null) {
+
+            if (set.has(curr.data) == false) {
+                set.add(curr.data)
+
+                prev = curr;
+                curr = next;
+
+                if (next) {
+                    next = next.next;
+                }
+
+            }
+            else {
+                if (prev) {
+                    prev.next = next;
+                }
+                else {
+                    head = next;
+                }
+                prev.next = next;
+                curr = next;
+
+                if (next) {
+                    next = next.next;
+                }
+            }
+        }
+
+        return head;
+    }
+}
+
---
+++ LinkedList/detectCycleInTheLinkedList.ts
@@ -0,0 +1,22 @@
+
+// Let A, B and C be three distances.
+
+// C is the length of cycle.
+
+// C = B + Some_Constant - (1)
+
+// 2(A + YC + B) = A + XC + B
+
+// A + C(2Y - X) + B = 0
+
+// A + B = C(X - 2Y)
+
+// A + B = CK - (2)
+
+//Equating equaltion (1) and (2) by assuming value of K as 1.
+
+// C = B + Some_Constant
+// C = B + A
+
+//we can clearly see that Some_Constant is equal to A.
+//So, A + B = C // Proved
