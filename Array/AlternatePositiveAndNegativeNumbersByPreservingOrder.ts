7772ecf156d28c2b47a18210727d74142fd868b2
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-10-05 15:24:31 +0530

    pull from origin

--- Array/AlternatePositiveAndNegativeNumbersByPreservingOrder.ts
+++ Array/AlternatePositiveAndNegativeNumbersByPreservingOrder.ts
@@ -3,11 +3,99 @@
 class AlternatePositiveAndNegativeNumbersByPreservingOrder {
     rearrange(arr: number[], n: number) {
 
+        let positiveCount = 0;
+        let negativeCount = 0;
 
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                positiveCount++;
+            }
+            else {
+                negativeCount++;
+            }
+        }
+
+        let start = -1;
+        let end = -1;
+
+        if (positiveCount > negativeCount) {
+
+            for (let i = 0; i < arr.length; i++) {
+
+                if (i % 2 != 0 && arr[i] >= 0) {
+
+                    let index = i;
+
+                    while (arr[index] >= 0) {
+                        index++;
+                    }
+
+                    this.rightRotate(arr, i, index);
+
+                }
+                else if (i % 2 == 0 && arr[i] < 0) {
+
+                    let index = i;
+
+                    while (arr[index] < 0) {
+                        index++;
+                    }
+
+                    this.rightRotate(arr, i, index);
+
+                }
+
+            }
+
+
+
+        } else {
+
+            for (let i = 0; i < arr.length; i++) {
+
+                if (i % 2 == 0 && arr[i] >= 0) {
+
+                    let index = i;
+
+                    while (arr[index] >= 0) {
+                        index++;
+                    }
+
+                    this.rightRotate(arr, i, index);
+
+                }
+                else if (i % 2 != 0 && arr[i] < 0) {
+
+                    let index = i;
+
+                    while (arr[index] < 0) {
+                        index++;
+                    }
+
+                    this.rightRotate(arr, i, index);
+
+                }
+
+            }
+
+
+        }
+
+        return arr;
     }
 
     rightRotate(arr: number[], i: number, j: number) {
 
+        let lastData = arr[j];
+        let prevData = arr[i];
+
+        for (let a = i; a < j; a++) {
+            let currData = arr[i + 1];
+            arr[i + 1] = prevData;
+            prevData = currData;
+        }
+
+        arr[i] = lastData;
     }
 
 }
