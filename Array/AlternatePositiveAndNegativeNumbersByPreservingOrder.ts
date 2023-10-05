12d139056abc9bb17b60404b01d8b9618d1f3891
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-10-05 15:01:08 +0530

    changes done

---
+++ Array/AlternatePositiveAndNegativeNumbersByPreservingOrder.ts
@@ -0,0 +1,17 @@
+
+
+class AlternatePositiveAndNegativeNumbersByPreservingOrder {
+    rearrange(arr: number[], n: number) {
+
+
+    }
+
+    rightRotate(arr: number[], i: number, j: number) {
+
+    }
+
+}
+
+let input_array = [-5, -2, 5, -2, 4, -7, 1, 8, 0, -8];
+
+console.log(new AlternatePositiveAndNegativeNumbersByPreservingOrder().rearrange(input_array, input_array.length));
