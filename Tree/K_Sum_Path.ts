613a62db4fca5658d6a2b4897350028dfece136a
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-10-18 15:37:09 +0530

    changes done

--- Tree/K_Sum_Path.ts
+++ Tree/K_Sum_Path.ts
@@ -1,40 +1,48 @@
-class K_Sum_Path
-{ 
-
-  count = 0;
-  sumK(root:TreeNode,K:number){
-     //code here
-     let path:number[] = [];
-     this.solve(root,K,path);
-     return this.count;
-  }
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function pathSum(root: TreeNode | null, targetSum: number): number {
+    let count = {count:0};
+    solve(root,targetSum,[],count);
+    return count.count;
+  };
   
-  solve(root:TreeNode,k:number,path:number[])
-  {
-     
-     if(root == null)
-     {
-         return;
-     }
-     path.push(root.data);
-     
-     this.solve(root.left,k,path);
-     this.solve(root.right,k,path);
-     
-      let j = path.length - 1;
-      let sum = 0;
-         
-      while(j >= 0)
-      {
-        sum = sum + path[j];
-        j--;
-        
-        if(sum == k)
+    function solve(root : TreeNode | null,k:number,path:number[],count:{count:number})
+    {
+       
+       if(root == null)
+       {
+           return;
+       }
+       path.push(root.data);
+       
+       solve(root.left,k,path,count);
+       solve(root.right,k,path,count);
+       
+        let j = path.length - 1;
+        let sum = 0;
+           
+        while(j >= 0)
         {
-          this.count++;
+          sum = sum + path[j];
+          j--;
+          
+          if(sum == k)
+          {
+            count.count++;
+          }
         }
-      }
-      
-      path.pop();
-  }
-} 
+        
+        path.pop();
+    }
+  
