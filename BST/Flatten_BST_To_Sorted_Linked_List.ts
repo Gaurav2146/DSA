09fc2e27fc2a3e815a322a57901bfd8f44aa9a7f
gaurav2146 <gaurav2146.gk@gmail.com>
2023-10-24 21:50:42 +0530

    changes done

--- BST/Flatten_BST_To_Sorted_Linked_List.ts
+++ BST/Flatten_BST_To_Sorted_Linked_List.ts
@@ -1,74 +1,65 @@
-     class BSTNode
-     {
-         
-             data:number;
-             left:BSTNode;
-             right:BSTNode;
+class BSTNode {
 
-            constructor(data:number)
-            {
-                this.data = data;
-                this.left = null;
-                this.right = null;
-            }
-     };
+    data: number;
+    left: BSTNode;
+    right: BSTNode;
 
-     class Flatten_BST_To_Sorted_Linked_List
-     {
+    constructor(data: number) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+};
 
-         head:BSTNode = new BSTNode(-1);
-         tail:BSTNode = new BSTNode(-1);
+class Flatten_BST_To_Sorted_Linked_List {
 
-         flatten(root:BSTNode):BSTNode
-         {
-             this.preorder(root);
-    
-             return this.head.right;
-         }
-     
-         preorder(root:BSTNode)
-         {
-     
-             if(root==null)
-             {
-               return;
-             }
-     
-             this.preorder(root.left );
+    head: BSTNode = new BSTNode(-1);
+    tail: BSTNode = new BSTNode(-1);
 
-             let right_node = root.right;
-     
-             if(this.head.data == -1)
-             {
-                 this.head.data = 1;
-                 this.head.right = root;
-                 this.tail = root;
-             }
-             else
-             {
-                root.left = null;
-                root.right = null;
-                this.tail.right = root;
-                this.tail = root;
-             }
-     
-             this.preorder(right_node);
-         }
-     }
+    flatten(root: BSTNode): BSTNode {
+        this.preorder(root);
 
-     let tree = new BSTNode(10);
-     tree.left = new BSTNode(6);
-     tree.left.left = new BSTNode(2);
-     tree.left.right = new BSTNode(8);
-     tree.right = new BSTNode(12);
-     tree.right.left = new BSTNode(11);
-     tree.right.right = new BSTNode(15);
+        return this.head.right;
+    }
 
-     let res = new Flatten_BST_To_Sorted_Linked_List().flatten(tree);
+    preorder(root: BSTNode) {
 
-     while(res!=null)
-     {
-        console.info(res.data);
-        res = res.right;
-     }
+        if (root == null) {
+            return;
+        }
+
+        this.preorder(root.left);
+
+        let right_node = root.right;
+
+        if (this.head.data == -1) {
+            this.head.data = 1;
+            this.head.right = root;
+            this.tail = root;
+        }
+        else {
+            root.left = null;
+            root.right = null;
+            this.tail.right = root;
+            this.tail = root;
+        }
+
+        this.preorder(right_node);
+    }
+}
+
+let tree = new BSTNode(10);
+tree.left = new BSTNode(6);
+tree.left.left = new BSTNode(2);
+tree.left.right = new BSTNode(8);
+tree.right = new BSTNode(12);
+tree.right.left = new BSTNode(11);
+tree.right.right = new BSTNode(15);
+
+let res = new Flatten_BST_To_Sorted_Linked_List().flatten(tree);
+
+while (res != null) {
+    console.info(res.data);
+    res = res.right;
+}
 
