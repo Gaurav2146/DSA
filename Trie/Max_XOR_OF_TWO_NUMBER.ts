b3c036a145549d20188866a245cf2a07b31aa907
gaurav2146 <gaurav2146.gk@gmail.com>
2023-10-30 22:10:21 +0530

    chnages done

--- Trie/Max_XOR_OF_TWO_NUMBER.ts
+++ Trie/Max_XOR_OF_TWO_NUMBER.ts
@@ -1,244 +1,205 @@
-// class Trie {
 
-//     value: string;
-//     children: Trie[];
-//     wordEnd: number = 0;
+function findMaximumXOR(nums: number[]): number {
 
-//     constructor() {
-//         this.children = new Array(2).fill(null);
-//     }
-// }
+    let result = findMaxValueAndMaxSizeOfBits(nums);
+    let length_of_bits = result.length;
+    let nusBitsRepresentation = new Array(nums.length).fill("0");
 
+    for (let i = 0; i < nums.length; i++) {
+        let res = findBitRepresentation(nums[i]);
 
-// function findMaximumXOR(nums: number[]): number {
+        let len = res.length;
 
-//     let result = findMaxValueAndMaxSizeOfBits(nums);
+        for (let j = 0; j < length_of_bits - len; j++) {
+            res = "0" + res;
+        }
 
-//     let length_of_bits = result.length;
+        nusBitsRepresentation[i] = res;
+    }
 
-//     let nusBitsRepresentation = new Array(nums.length).fill("0");
+    let trieRoot = new Trie();
 
-//     for (let i = 0; i < nums.length; i++) {
-//         let res = findBitRepresentation(nums[i]);
+    for (let i = 0; i < nusBitsRepresentation.length; i++) {
+        insertIntoTrie(trieRoot, nusBitsRepresentation[i], 0);
+    }
 
-//         let len = res.length;
+    let first_number;
+    let max = { max: Number.MIN_SAFE_INTEGER };
 
-//         for (let j = 0; j < length_of_bits - len; j++) {
-//             res = "0" + res;
-//         }
-
-//         nusBitsRepresentation[i] = res;
-//     }
-
-//     console.log(nums , "nums");
-//     console.log(nusBitsRepresentation , "nusBitsRepresentation");
-
-//     let trieRoot = new Trie();
-
-//     for (let i = 0; i < nusBitsRepresentation.length; i++) {
-//         insertIntoTrie(trieRoot, nusBitsRepresentation[i], 0);
-//     }
-
-//     console.log(trieRoot , "trieRoot");
-
-//     printTrie(trieRoot);
-
-//     let first_number;
-//     let max = { max: Number.MIN_SAFE_INTEGER };
-
-//     for (let i = 0; i < nusBitsRepresentation.length - 1; i++) {
-//         first_number = nums[i];
-//         let not_value = findNegation(nusBitsRepresentation[i]);
-//         let found_Index = -1;
-//         let any_children = -1;
-
-//         for (let j = 0; j < trieRoot.children.length; j++) {
-//             if (trieRoot.children[j]) {
-//                 any_children = j;
-//             }
-
-//             if (trieRoot.children[j] && trieRoot.children[j].value == not_value[0]) {
-//                 found_Index = j;
-//             }
-//         }
-
-//         if (found_Index != -1) {
-//             searchTrie(trieRoot.children[found_Index], not_value, 1, first_number, trieRoot.children[found_Index].value, max);
-//         }
-//         else if (any_children != -1) {
-//             searchTrie(trieRoot.children[any_children], not_value, 1, first_number, trieRoot.children[any_children].value, max);
-//         }
-//     }
-
-//     return max.max;
-// };
-
-// function searchTrie(root: Trie, input: string, index: number, first_number: number, output: string, max: { max: number }) {
-
-//     if (root == null) {
-//         return;
-//     }
-
-//     if (index > input.length) {
-//         return;
-//     }
-
-//     if (input.length == output.length) {
-
-//         let data = convertBitToNumber(output);
+    for (let i = 0; i < nusBitsRepresentation.length - 1; i++) {
+        first_number = nums[i];
+        let not_value = findNegation(nusBitsRepresentation[i]);
+       
+        searchTrie(trieRoot, not_value, 1, first_number, "", max);
         
-//         let xor = data ^ first_number;
+    }
 
-//         console.log("data is " + data + " and first_number is " + first_number);
+    return max.max;
+};
+
+function searchTrie(root: Trie, input: string, index: number, first_number: number, output: string, max: { max: number }) {
+
+    if (root == null) {
+        return;
+    }
+
+    if (index > input.length) {
+        return;
+    }
+
+    if (input.length == output.length) {
+
+        let data = convertBitToNumber(output);
         
-//         console.log( "input is " + input + " and output is " + output);
+        let xor = data ^ first_number;
 
-//         console.log(xor, "xor");
+        console.log("data is " + data + " and first_number is " + first_number);
+        
+        console.log( "input is " + input + " and output is " + output);
 
-//         if (xor > max.max) {
-//             max.max = xor;
-//         }
-//         return;
-//     }
+        console.log(xor, "xor");
 
-//     let found = -1;
-//     let lastFound = -1;
+        if (xor > max.max) {
+            max.max = xor;
+        }
+        return;
+    }
 
-//     for (let i = 0; i < root.children.length; i++) {
-//         if (root.children[i]) {
-//             lastFound = i;
-//         }
+    let found = -1;
+    let lastFound = -1;
 
-//         if (root.children[i] && root.children[i].value == input[index]) {
-//             found = i;
-//             break;
-//         }
-//     }
+    for (let i = 0; i < root.children.length; i++) {
+        if (root.children[i]) {
+            lastFound = i;
+        }
 
-//     if (found == -1) {
-//         if (lastFound == -1) {
-//             let data = convertBitToNumber(output);
-//             let xor = data ^ first_number;
-//             console.log(xor, "xor");
-//             if (xor > max.max) {
-//                 max.max = xor;
-//             }
-//             return;
-//         }
-//         else {
-//             searchTrie(root.children[lastFound], input, index + 1, first_number, output + root.children[lastFound].value, max);
-//         }
-//     }
-//     else {
-//         searchTrie(root.children[found], input, index + 1, first_number, output + root.children[found].value, max);
-//     }
-// }
+        if (root.children[i] && root.children[i].value == input[index]) {
+            found = i;
+            break;
+        }
+    }
 
-// function convertBitToNumber(input: string) {
-//     let len = input.length - 1;
-//     let res = 0;
-//     let reference = 1;
+    if (found == -1) {
+        if (lastFound == -1) {
+            let data = convertBitToNumber(output);
+            let xor = data ^ first_number;
+            console.log(xor, "xor");
+            if (xor > max.max) {
+                max.max = xor;
+            }
+            return;
+        }
+        else {
+            searchTrie(root.children[lastFound], input, index + 1, first_number, output + root.children[lastFound].value, max);
+        }
+    }
+    else {
+        searchTrie(root.children[found], input, index + 1, first_number, output + root.children[found].value, max);
+    }
+}
 
-//     while (len >= 0) {
-//         res = res + reference * Number(input[len]);
-//         reference = reference * 2;
-//         len--;
-//     }
+function convertBitToNumber(input: string) {
+    let len = input.length - 1;
+    let res = 0;
+    let reference = 1;
 
-//     return res;
-// }
+    while (len >= 0) {
+        res = res + reference * Number(input[len]);
+        reference = reference * 2;
+        len--;
+    }
 
-// function insertIntoTrie(root: Trie, input: string, index: number) {
+    return res;
+}
 
-//     if (input.length == index && root.value != undefined) {
-//         root.wordEnd = root.wordEnd + 1;
-//         return;
-//     }
-//     else if (input.length == index && root.value == undefined) {
-//         let node = new Trie();
-//         node.value = input[index];
-//         root.children[Number(input[index])] = node;
-//         return;
-//     }
+function insertIntoTrie(root:Trie,index:number,input:string) {
 
-//     let foundIndex = -1;
+    if(index == input.length)
+    {
+       root.isEndOfString = true;
+       return;
+    }
 
-//     for (let i = 0; i < root.children.length; i++) {
-//         if (root.children[i] && root.children[i].value == input[index]) {
-//             foundIndex = i;
-//             break;
-//         }
-//     }
+    let element_to_insert = input[index];
 
-//     if (foundIndex >= 0 && input.length - 1 == index) {
-//         root.children[foundIndex].wordEnd = root.children[foundIndex].wordEnd + 1;
-//     }
-//     else {
-//         let node = new Trie();
-//         node.value = input[index];
-//         root.children[Number(input[index])] = node;
+    let index_to_insert = Number(element_to_insert);
 
-//         insertIntoTrie(node, input, index + 1);
-//     }
+    if(root.children[index_to_insert] == null)
+    {
+       let node = new Trie();
+       node.value = element_to_insert;
+       root.children[index_to_insert] = node;
 
-// }
+       insertIntoTrie(root.children[index_to_insert],index + 1,input);
+    }
+    else
+    {
+      insertIntoTrie(root.children[index_to_insert],index + 1,input);
+    }
 
-// function findNegation(input: string) {
-//     let res = "";
+}
 
-//     for (let i = 0; i < input.length; i++) {
-//         if (input[i] == "1") {
-//             res = res + "0";
-//         }
-//         else {
-//             res = res + "1";
-//         }
-//     }
-//     return res;
-// }
+function findNegation(input: string) {
+    let res = "";
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == "1") {
+            res = res + "0";
+        }
+        else {
+            res = res + "1";
+        }
+    }
+    return res;
+}
 
 
-// function findMaxValueAndMaxSizeOfBits(nums: number[]): string {
+function findMaxValueAndMaxSizeOfBits(nums: number[]): string {
 
-//     let max = Number.MIN_SAFE_INTEGER;
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] > max) {
-//             max = nums[i];
-//         }
-//     }
+    let max = Number.MIN_SAFE_INTEGER;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) {
+            max = nums[i];
+        }
+    }
 
-//     return findBitRepresentation(max);
-// }
+    return findBitRepresentation(max);
+}
 
-// function findBitRepresentation(num: number): string {
-//     let res = "";
-//     while (num != 0) {
-//         res = num % 2 + res;
+function findBitRepresentation(num: number): string {
+    let res = "";
+    while (num != 0) {
+        res = num % 2 + res;
 
-//         num = Math.floor(num / 2);
-//     }
-//     return res;
-// }
+        num = Math.floor(num / 2);
+    }
+    return res;
+}
 
-// function printTrie(root:Trie)
-// {
-//     console.log(root.value);
-//     if(root.wordEnd > 0)
-//     {
-//         console.log("==========================================");
-//         return;
-//     }
+function printTrie(root:Trie,res:string,allResults:string[])
+{
+    if(root == null)
+    {
+      return;
+    }
 
-//     for(let i=0; i < root.children.length; i++)
-//     {
-//         if(root.children[i])
-//         {
-//             printTrie(root.children[i]);
-//         }
-//     }
-// }
+   if(root.isEndOfString == true)
+   {
+    allResults.push(res);
+    return;
+   }
+
+   for(let i=0; i < root.children.length; i++)
+   {
+
+    if(root.children[i] && root.children[i].value)
+    {
+        printTrie(root.children[i],res+root.children[i].value,allResults);
+    }
+   }
+}
 
 
-// console.log(findMaximumXOR([14,70,53,83,49,91,36,80,92,51,66,70]));
+console.log(findMaximumXOR([14,70,53,83,49,91,36,80,92,51,66,70]));
 
 
