ddb07c53e4a97d1894a3b4ade053cdb8a3dd8721
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-10-31 13:17:52 +0530

    changes done

--- Trie/Recursive_Implementation.ts
+++ Trie/Recursive_Implementation.ts
@@ -1,7 +1,6 @@
-class recursiveTrieNode
-{
-    left_children:recursiveTrieNode|null;
-    right_children:recursiveTrieNode|null;
+class recursiveTrieNode {
+    left_children: recursiveTrieNode | null;
+    right_children: recursiveTrieNode | null;
 
     constructor() {
         this.left_children = null;
@@ -9,104 +8,84 @@ class recursiveTrieNode
     }
 }
 
-class Recursive_Implementation
-{
-   insert(root:recursiveTrieNode,num:number)
-   {
-    for(let i=31; i >=0; i--)
-    {
-        //Checking if ith bit is 0 or 1.
-        let bit:number = (num >> i) & 1;
+class Recursive_Implementation {
+    insert(root: recursiveTrieNode, num: number) {
+        for (let i = 31; i >= 0; i--) {
+            //Checking if ith bit is 0 or 1.
+            let bit: number = (num >> i) & 1;
 
-        if(bit == 0)
-        {
-            if(root.left_children != null)
-            {
-                root = root.left_children;
+            if (bit == 0) {
+                if (root.left_children != null) {
+                    root = root.left_children;
+                }
+                else {
+                    let node = new recursiveTrieNode();
+                    root.left_children = node;
+                    root = root.left_children;
+                }
             }
-            else
-            {
-                let node = new recursiveTrieNode();
-                root.left_children = node;
-                root = root.left_children;
-            }
-        }
-        else
-        {
-            if(root.right_children != null)
-            {
-                root = root.right_children;
-            }
-            else
-            {
-                let node = new recursiveTrieNode();
-                root.right_children = node;
-                root = root.right_children;
-            }
-        }
-    }  
-   }
-
-   findMaxXor(root:recursiveTrieNode , num:number):number
-   {
-
-    let xor = 0;
-
-    for(let i=31; i >=0; i--)
-    {
-        //Checking if ith bit is 0 or 1.
-        let bit:number = (num >> i) & 1;
-
-        if(bit == 0)
-        {
-          if(root.right_children != null)
-          {
-            xor = xor + (1 << i);
-            root = root.right_children
-          }
-          else
-          {
-            root = root.left_children;
-          }
-        }
-        else
-        {
-            if(root.left_children != null)
-            {
-                xor = xor + (1 << i);
-              root = root.left_children;
-            }
-            else
-            {
-              root = root.right_children;
+            else {
+                if (root.right_children != null) {
+                    root = root.right_children;
+                }
+                else {
+                    let node = new recursiveTrieNode();
+                    root.right_children = node;
+                    root = root.right_children;
+                }
             }
         }
     }
 
-    return xor;
-   }
+    findMaxXor(root: recursiveTrieNode, num: number): number {
 
-   insertArray(nums:number[])
-   {
-    let trieNodeRecursive = new recursiveTrieNode();
+        let xor = 0;
 
-    for(let num of nums)
-    {
-       this.insert(trieNodeRecursive,num);
+        for (let i = 31; i >= 0; i--) {
+            //Checking if ith bit is 0 or 1.
+            let bit: number = (num >> i) & 1;
+
+            if (bit == 0) {
+                if (root.right_children != null) {
+                    xor = xor + (1 << i);
+                    root = root.right_children
+                }
+                else {
+                    root = root.left_children;
+                }
+            }
+            else {
+                if (root.left_children != null) {
+                    xor = xor + (1 << i);
+                    root = root.left_children;
+                }
+                else {
+                    root = root.right_children;
+                }
+            }
+        }
+
+        return xor;
     }
 
-    let max_xor = Number.MIN_SAFE_INTEGER;
+    insertArray(nums: number[]) {
+        let trieNodeRecursive = new recursiveTrieNode();
 
-    for(let num of nums)
-    {
-        max_xor = Math.max(max_xor , this.findMaxXor(trieNodeRecursive,num));
+        for (let num of nums) {
+            this.insert(trieNodeRecursive, num);
+        }
+
+        let max_xor = Number.MIN_SAFE_INTEGER;
+
+        for (let num of nums) {
+            max_xor = Math.max(max_xor, this.findMaxXor(trieNodeRecursive, num));
+        }
+
+        return max_xor;
     }
-
-    return max_xor;
-   }
 }
 
-console.log( "Recursive_Implementation", new Recursive_Implementation().insertArray([14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70]));
+console.log("Recursive_Implementation", new Recursive_Implementation().insertArray([14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70]));
 
 
 
