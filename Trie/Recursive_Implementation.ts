7b6a21142c49af8cf752a0c30328127cd53b2538
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-10-31 12:14:16 +0530

    chnanges done

--- Trie/Recursive_Implementation.ts
+++ Trie/Recursive_Implementation.ts
@@ -1,7 +1,45 @@
+class recursiveTrieNode
+{
+    value:number;
+    children:recursiveTrieNode[];
+    isEnd:boolean;
+
+    constructor() {
+        this.children = new Array(2).fill(null);
+        this.isEnd = false;
+    }
+}
+
 class Recursive_Implementation
 {
+   insert(root:recursiveTrieNode,num:number)
+   {
+    for(let i=31; i >=0; i--)
+    {
+        //Checking if ith bit is 0 or 1.
+        let bit:number = (num >> i) & 1;
 
-    
+        let node = new recursiveTrieNode();
+        node.value = bit;
+        root.children[bit] = node;
+        root = root.children[bit];
+    }
+       
+     root.isEnd = true; 
+   }
 
+   insertArray(nums:number[])
+   {
+    let trieNodeRecursive = new recursiveTrieNode();
 
+    for(let num of nums)
+    {
+       this.insert(trieNodeRecursive,num);
+    }
+   }
 }
+
+
+
+
+
