e60d15b8900bd816fd7203619e1febfac680d318
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-11-02 15:45:19 +0530

    changes done

--- Jump_Game/min_number_of_jumps.ts
+++ Jump_Game/min_number_of_jumps.ts
@@ -1,20 +1,27 @@
 function jump(nums: number[]): number {
-    return min_jumps(nums,0,0);
-   };
-   
-   function min_jumps(nums:number[],output:number,index:number)
-   {
-      
-     if(index == nums.length - 1)
-     {
-         return output;
-     }
-   
-     let min = Number.MAX_SAFE_INTEGER;
-   
-     for(let i=1; i <= nums[index]; i++)
-     {
-       min = Math.min(min,min_jumps(nums,output+1,index+i));
-     }
-      return min;
-   }
+
+    let DP = new Map();
+
+    return min_jumps(nums, 0, 0, DP);
+};
+
+function min_jumps(nums: number[], output: number, index: number, DP: Map<string, number>) {
+
+    if (index >= nums.length - 1) {
+        return output;
+    }
+
+    let key = output + "-" + index;
+
+    if (DP.has(key)) {
+        return DP.get(key);
+    }
+
+    let min = Number.MAX_SAFE_INTEGER;
+
+    for (let i = 1; i <= nums[index]; i++) {
+        min = Math.min(min, min_jumps(nums, output + 1, index + i, DP));
+    }
+    DP.set(key, min);
+    return min;
+}
