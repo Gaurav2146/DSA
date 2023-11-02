d2e189c2f2b77ee660b2871e1a23f0d8fde3fa4d
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-11-02 12:10:07 +0530

    changes done

--- BIT_Manupulation/Find_Number_in_which_all_numbers_are_repeated_two_times_except_one_number.ts
+++ BIT_Manupulation/Find_Number_in_which_all_numbers_are_repeated_two_times_except_one_number.ts
@@ -1,8 +1,14 @@
 //You have to Find a Number from an array in which all numbers are repeated three times except one number.
 
 class numberRepeatedTwoTimes {
-
-    
-
-
+    findNumber(input:number[])
+    {
+        let result = 0;
+        for(let i=0; i < input.length; i++)
+        {
+            result = result ^ input[i];
+        }
+        return result;
+    }
 }
+ console.log( new numberRepeatedTwoTimes().findNumber([1,1,2,3,4,3,4]) );
