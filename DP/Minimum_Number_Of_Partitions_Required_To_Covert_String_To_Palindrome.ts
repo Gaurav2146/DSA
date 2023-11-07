95834dfb223f214b9f7be0fd9258f60745838055
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-11-07 14:54:44 +0530

    changes done

--- DP/Minimum_Number_Of_Partitions_Required_To_Covert_String_To_Palindrome.ts
+++ DP/Minimum_Number_Of_Partitions_Required_To_Covert_String_To_Palindrome.ts
@@ -1,57 +1,64 @@
+// Palindrome Partitioning
 
-class Minimum_Number_Of_Partitions_Required_To_Covert_String_To_Palindrome
-{
+// Given a string str, a partitioning of the string is a palindrome partitioning if every 
+//sub-string of the partition is a palindrome, the task is to find the minimum number of
+// cuts needed for palindrome partitioning of the given string.
 
-    calculateMinimumCutsRequires(str:string)
-    {
-      this.solve(str,0,str.length-1);
+// Palindrome Partition
+
+// Examples :  
+
+// Input: str = “geek” 
+// Output: 2 
+// Explanation: We need to make minimum 2 cuts, i.e., “g ee k”
+
+// Input: str = “aaaa” 
+// Output: 0 
+// Explanation: The string is already a palindrome.
+
+class Minimum_Number_Of_Partitions_Required_To_Covert_String_To_Palindrome {
+
+    calculateMinimumCutsRequires(str: string) {
+        this.solve(str, 0, str.length - 1);
     }
 
-    solve(s:string,start:number,end:number)
-    {
-        if(start >= end)
-        {
+    solve(s: string, start: number, end: number) {
+        if (start >= end) {
             return 0;
         }
 
-        if(this.isPalindrome(s.substring(start,end+1)) == true)
-        {
-          return 0;
+        if (this.isPalindrome(s.substring(start, end + 1)) == true) {
+            return 0;
         }
 
         let min = Number.MAX_SAFE_INTEGER;
 
-        for(let k=start; k < end; k++)
-        {
-           let res = 1 + this.solve(s,start,k+1) + this.solve(s,k+1,end);
+        for (let k = start; k < end; k++) {
+            let res = 1 + this.solve(s, start, k + 1) + this.solve(s, k + 1, end);
 
-           if(res < min)
-           {
-            res = min;
-           }
+            if (res < min) {
+                res = min;
+            }
         }
 
         return min;
     }
 
-    isPalindrome(s:string)
-    {
-        let i=0;
-        let j = s.length -1;
-      
+    isPalindrome(s: string) {
+        let i = 0;
+        let j = s.length - 1;
+
         let flag = true;
-      
-        while(i <= j)
-        {
-          if(s[i]!=s[j])
-          {
-              flag = false;
-              break;
-          }
-          i++;
-          j--;
+
+        while (i <= j) {
+            if (s[i] != s[j]) {
+                flag = false;
+                break;
+            }
+            i++;
+            j--;
         }
-      
+
         return flag;
     }
 }
