df75412b66f567833a961df0d911155ba3f80d65
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-11-08 11:22:01 +0530

    changes done

--- Graph/CycleDetection/directedGraph.ts
+++ Graph/CycleDetection/directedGraph.ts
@@ -9,12 +9,11 @@
 //===================================== DFS =================================================================
 //Create Adjecency list for your graph.
 //Create a DFS Visited map which will store the node on which DFS is already done.
-//Create a visited node map which will tell if the node is visisted or not.
 
 //Imp - when we will return after DFS call then we will mark DFS visited to false for that node.
 
-// 1. If we find any adjecent node which is already visited and is also present is DFS visited map then it's a cycle.
-// 2. If we find any adjecent node which is vsited but it is not present in DFS visited then we will ignore that node.
+// 1. If we find any adjecent node which is already present is DFS visited map then it's a cycle.
+// 2. If we find any adjecent node which is not present in DFS visited then we will call DFS on that node.
 //===========================================================================================================
 
 class CycleDetectionInDirectedGraph
