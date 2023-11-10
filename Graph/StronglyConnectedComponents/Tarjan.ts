9e1c114be3779d8887fe3827e6e4a3acc6a11604
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-11-10 14:10:47 +0530

    chnages done

--- Graph/StronglyConnectedComponents/Tarjan.ts
+++ Graph/StronglyConnectedComponents/Tarjan.ts
@@ -18,8 +18,110 @@
 
 class Tarjan
 {
+    adjecencyMetrix: number[][];
+    size: number;
 
-    
+    constructor(size: number) {
+        this.adjecencyMetrix = new Array(size);
+        this.size = size;
 
+        for (let i = 0; i < this.adjecencyMetrix.length; i++) {
+            this.adjecencyMetrix[i] = new Array(size).fill(0);
+        }
+    }
 
+    addEdge(u: number, v: number) {
+        this.adjecencyMetrix[u][v] = 1;
+    }
+
+    findStronglyConnectedComponents()
+    {
+        let stronglyConnectedComponents:number[][] = [];
+        let low = new Array(this.size).fill(-1);
+        let discovery = new Array(this.size).fill(-1);
+        let stack:number[] = [];
+        let stackFlag = new Array(this.size).fill(false);
+        let vertex = this.findVerticesOfGraph();
+        let src = vertex[0];
+        this.traverse(src,low,discovery,stack,stackFlag,0,stronglyConnectedComponents);
+        return stronglyConnectedComponents;
+    }
+
+    findVerticesOfGraph(): number[] {
+        let vertices = [];
+        for (let i = 0; i < this.size; i++) {
+            vertices.push(i);
+        }
+        return vertices;
+    }
+
+    traverse(vertex:number,low:number[],discovery:number[],stack:number[],stackFlag:boolean[],time:number,stronglyConnectedComponents:number[][])
+    {
+       low[vertex] = time;
+       discovery[vertex] = time;
+       stack.push(vertex);
+       stackFlag[vertex] = true;
+
+       for(let i=0; i < this.size; i++)
+       {
+          if(this.adjecencyMetrix[vertex][i] == 1)
+          {
+            //Cross Edge
+            if( discovery[i] != -1 && stackFlag[i] == false)
+            {
+                continue;
+            } 
+            //Back edge
+            else if(discovery[i] != -1 && stackFlag[i] == true)
+            {
+               low[vertex] = Math.min(discovery[i],low[vertex]);
+            }
+            else if(discovery[i] == -1)
+            {
+                this.traverse(i,low,discovery,stack,stackFlag,time+1,stronglyConnectedComponents);
+
+                low[vertex] = Math.min(low[vertex],low[i]);
+            }
+          }
+       }
+
+        //Current node is root node
+        if(low[vertex] == discovery[vertex])
+        {
+            console.log(vertex , "Root Node");
+            let component = [];
+
+            while( stack.length > 0 && stack[stack.length - 1] != vertex)
+            {
+                let ele = stack.pop();
+                stackFlag[stack.length - 1] = false;
+                component.push(ele);
+            }
+
+            if(stack.length > 0)
+            {
+                let vertex_ele = stack.pop();
+                stackFlag[stack.length - 1] = false;
+                component.push(vertex_ele);
+            }
+
+            stronglyConnectedComponents.push(component);
+        } 
+    }
 }
+
+
+let tarjan = new Tarjan(8);
+
+tarjan.addEdge(0, 1);
+tarjan.addEdge(1, 2);
+tarjan.addEdge(2, 0);
+tarjan.addEdge(2, 3);
+tarjan.addEdge(3, 4);
+tarjan.addEdge(4, 5);
+tarjan.addEdge(5, 6);
+tarjan.addEdge(6, 4);
+tarjan.addEdge(4, 7);
+tarjan.addEdge(6, 7);
+
+console.log("Strongly connected components are ", tarjan.findStronglyConnectedComponents());
