fd0479423cff616d5b0f5d1e8c02518a94eb416c
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-11-10 15:35:53 +0530

    chnages done

--- Graph/ArticulationPoint/Bridge_In_Graph.ts
+++ Graph/ArticulationPoint/Bridge_In_Graph.ts
@@ -1,5 +1,4 @@
-class Bridge_In_Graph
-{
+class Bridge_In_Graph {
     adjecencyMetrix: number[][];
     size: number;
 
@@ -17,16 +16,15 @@ class Bridge_In_Graph
         this.adjecencyMetrix[v][u] = 1;
     }
 
-    findBridges()
-    {
-        let bridges:number[][] = [];
+    findBridges() {
+        let bridges: number[][] = [];
         let low = new Array(this.size).fill(-1);
         let discovery = new Array(this.size).fill(-1);
         let parent = new Array(this.size).fill(-1);
-      
+
         let vertex = this.findVerticesOfGraph();
         let src = vertex[0];
-        this.traverse(src,low,discovery,parent,0,bridges);
+        this.traverse(src, low, discovery, parent, 0, bridges);
         return bridges;
     }
 
@@ -38,33 +36,27 @@ class Bridge_In_Graph
         return vertices;
     }
 
-    traverse(vertex:number,low:number[],discovery:number[],parent:number[],time:number,bridges:number[][])
-    {
-       low[vertex] = time;
-       discovery[vertex] = time;
-       
-       for(let i=0; i < this.size; i++)
-       {
-          if(this.adjecencyMetrix[vertex][i] == 1)
-          {
-            //Back edge
-            if(discovery[i] != -1 && parent[vertex] != i)
-            {
-               low[vertex] = Math.min(discovery[i],low[vertex]);
-            }
-            else if(discovery[i] == -1)
-            {
-                parent[i] = vertex;
-                this.traverse(i,low,discovery,parent,time+1,bridges);
-                low[vertex] = Math.min(low[vertex],low[i]);
-                
-                if(low[i] > discovery[vertex])
-                {
-                    bridges.push([i,vertex]);
+    traverse(vertex: number, low: number[], discovery: number[], parent: number[], time: number, bridges: number[][]) {
+        low[vertex] = time;
+        discovery[vertex] = time;
+
+        for (let i = 0; i < this.size; i++) {
+            if (this.adjecencyMetrix[vertex][i] == 1) {
+                //Back edge
+                if (discovery[i] != -1 && parent[vertex] != i) {
+                    low[vertex] = Math.min(discovery[i], low[vertex]);
+                }
+                else if (discovery[i] == -1) {
+                    parent[i] = vertex;
+                    this.traverse(i, low, discovery, parent, time + 1, bridges);
+                    low[vertex] = Math.min(low[vertex], low[i]);
+
+                    if (low[i] > discovery[vertex]) {
+                        bridges.push([i, vertex]);
+                    }
                 }
             }
-          }
-       }
+        }
     }
 }
 
